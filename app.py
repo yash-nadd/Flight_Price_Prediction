d24d81efc150cd8d5ab8306349fc36a42d75fb45
Yashwanth Naddipalli <yashwanth.naddipalli@gmail.com>
2024-09-19 22:21:41 +0530

    app

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
+from flask import Flask,url_for,render_template
 import pandas as pd
 import joblib
-from flask import (
-    Flask,
-    url_for,
-    render_template
-)
 from forms import InputForm
 
 app = Flask(__name__)
@@ -12,18 +8,18 @@ app.config["SECRET_KEY"] = "secret_key"
 
 model = joblib.load("model.joblib")
 
+
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template("home.html", title="Home")
+    return render_template("home.html",title = "Home")
 
-
-@app.route("/predict", methods=["GET", "POST"])
+@app.route("/predict",methods = ["GET","POST"])
 def predict():
     form = InputForm()
     if form.validate_on_submit():
-        x_new = pd.DataFrame(dict(
-            airline=[form.airline.data],
+        X_new = pd.DataFrame(dict(
+            airline = [form.airline.data],
             date_of_journey=[form.date_of_journey.data.strftime("%Y-%m-%d")],
             source=[form.source.data],
             destination=[form.destination.data],
@@ -33,12 +29,11 @@ def predict():
             total_stops=[form.total_stops.data],
             additional_info=[form.additional_info.data]
         ))
-        prediction = model.predict(x_new)[0]
+        prediction = model.predict(X_new)[0]
         message = f"The predicted price is {prediction:,.0f} INR!"
     else:
-        message = "Please provide valid input details!"
-    return render_template("predict.html", title="Predict", form=form, output=message)
-
+        message = f"Please provide valid input details"
+    return render_template("predict.html",title = "Predict",form = form,output = message)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug = True)
